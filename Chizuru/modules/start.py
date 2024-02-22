05d6f3365b38c9d1f849adc9d104308c6e9a01fe
ujjwal3155 <153728856+ujjwal3155@users.noreply.github.com>
2024-02-22 11:44:47 +0530

    Update start.py

--- Chizuru/modules/start.py
+++ Chizuru/modules/start.py
@@ -70,7 +70,7 @@ back_buttons  = [[
 
 @Chizuru.on_message(filters.command("start"))
 async def start(_,message):
-  await message.reply_photo("https://telegra.ph/file/fabb39ffadb15ac0697ab.jpg",
+  await message.reply_photo("https://t.me/mmaminhindi/1898",
                             caption=start_txt.format(message.from_user.mention),reply_markup=button)
 
 
